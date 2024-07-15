4e15ebb0204112aa5756b2ce898a33cd67742d69
g4rcez <allan.f.garcez@gmail.com>
2024-07-15 02:36:18 -0300

    feat: add drawer + modal/dialog in one component

--- src/components/floating/dropdown.tsx
+++ src/components/floating/dropdown.tsx
@@ -4,16 +4,16 @@ import {
     flip,
     FloatingArrow,
     FloatingFocusManager,
+    FloatingPortal,
     offset,
     shift,
     useClick,
     useDismiss,
     useFloating,
-    useId,
     useInteractions,
     useRole,
 } from "@floating-ui/react";
-import React, { Fragment, PropsWithChildren, useMemo, useRef, useState } from "react";
+import React, { Fragment, useId, PropsWithChildren, useMemo, useRef, useState } from "react";
 
 type DropdownProps = {
     open?: boolean;
@@ -24,25 +24,18 @@ type DropdownProps = {
 };
 
 export const Dropdown = (props: PropsWithChildren<DropdownProps>) => {
+    const headingId = useId();
     const [open, setOpen] = useState(props.open);
     const arrowRef = useRef(null);
     const middleware = useMemo(
-        () => [
-            offset(10),
-            flip({ fallbackAxisSideDirection: "end" }),
-            shift(),
-            arrow({
-                padding: 5,
-                element: arrowRef,
-            }),
-        ],
+        () => [offset(10), flip({ fallbackAxisSideDirection: "end" }), shift(), arrow({ padding: 5, element: arrowRef })],
         [props.arrow]
     );
     const { refs, floatingStyles, context } = useFloating({
         open,
+        middleware,
         transform: true,
         whileElementsMounted: autoUpdate,
-        middleware,
         onOpenChange: (nextValue, event) => {
             const element = (event as any)?.relatedTarget as HTMLElement;
             if (element) {
@@ -59,34 +52,34 @@ export const Dropdown = (props: PropsWithChildren<DropdownProps>) => {
 
     const { getReferenceProps, getFloatingProps } = useInteractions([click, dismiss, role]);
 
-    const headingId = useId();
-
     return (
         <Fragment>
-            <button ref={refs.setReference} {...getReferenceProps()}>
+            <button ref={refs.setReference} {...getReferenceProps()} type="button">
                 {props.trigger}
             </button>
             {open && (
-                <FloatingFocusManager returnFocus visuallyHiddenDismiss restoreFocus context={context} modal={false}>
-                    <div
-                        className="bg-floating-background isolate z-floating border shadow-2xl p-6 border-floating-border rounded-lg"
-                        ref={refs.setFloating}
-                        style={floatingStyles}
-                        aria-labelledby={headingId}
-                        {...getFloatingProps()}
-                    >
-                        <FloatingArrow
-                            ref={arrowRef}
-                            context={context}
-                            strokeWidth={0.1}
-                            className="fill-floating-background stroke-floating-border"
-                        />
-                        <header className="mb-2">
-                            <h3 className="leading-snug font-medium text-2xl tracking-tight text-left">{props.title}</h3>
-                        </header>
-                        {props.children}
-                    </div>
-                </FloatingFocusManager>
+                <FloatingPortal id={`${headingId}-portal`}>
+                    <FloatingFocusManager returnFocus visuallyHiddenDismiss restoreFocus context={context} modal={false}>
+                        <div
+                            className="bg-floating-background relative min-w-96 isolate z-floating border shadow-2xl p-6 border-floating-border rounded-lg"
+                            ref={refs.setFloating}
+                            style={floatingStyles}
+                            aria-labelledby={headingId}
+                            {...getFloatingProps()}
+                        >
+                            <FloatingArrow
+                                ref={arrowRef}
+                                context={context}
+                                strokeWidth={0.1}
+                                className="fill-floating-background stroke-floating-border"
+                            />
+                            <header className="mb-2">
+                                <h3 className="leading-snug font-medium text-2xl tracking-tight text-left">{props.title}</h3>
+                            </header>
+                            {props.children}
+                        </div>
+                    </FloatingFocusManager>
+                </FloatingPortal>
             )}
         </Fragment>
     );
--- src/components/floating/modal.tsx
+++ src/components/floating/modal.tsx
@@ -6,20 +6,50 @@ import {
     useClick,
     useDismiss,
     useFloating,
-    useId,
     useInteractions,
     useRole,
 } from "@floating-ui/react";
 import { Slot } from "@radix-ui/react-slot";
 import { cva } from "class-variance-authority";
-import React, { Fragment, PropsWithChildren } from "react";
+import { AnimatePresence, motion, MotionValue, PanInfo, TargetAndTransition, useMotionValue } from "framer-motion";
+import React, { Fragment, PropsWithChildren, useId } from "react";
 import { Label } from "../../types";
 
-const variants = cva("ring-0 outline-0 appearance-none container flex flex-col gap-4 flex-nowrap min-w-xs bg-floating-background", {
+type DrawerSides = "left" | "right";
+
+type AnimationLabels = "initial" | "enter" | "exit";
+
+type Animations = {
+    drawer: (type: DrawerSides) => Record<AnimationLabels, TargetAndTransition>;
+    dialog: Record<AnimationLabels, TargetAndTransition>;
+};
+
+const drawerLeft = {
+    initial: { left: "-48rem", opacity: 0.4 },
+    enter: { left: 0, opacity: 1 },
+    exit: { left: "-48rem", opacity: 0.4 },
+};
+
+const drawerRight = {
+    initial: { right: "-48rem", opacity: 0.4 },
+    enter: { right: 0, opacity: 1 },
+    exit: { right: "-48rem", opacity: 0.4 },
+};
+
+const animations: Animations = {
+    drawer: (type) => (type === "left" ? drawerLeft : drawerRight),
+    dialog: {
+        initial: { opacity: 0, scale: 0.9 },
+        enter: { opacity: 1, scale: [1.05, 1] },
+        exit: { opacity: 0, scale: 0.9 },
+    },
+};
+
+const variants = cva("isolate ring-0 outline-0 appearance-none container flex flex-col gap-4 flex-nowrap min-w-xs bg-floating-background", {
     variants: {
         type: {
             drawer: "max-h-screen h-screen min-h-0",
-            dialog: "max-h-[calc(100lvh-5%)] rounded-lg py-8",
+            dialog: "max-h-[calc(100lvh-10%)] h-[inherit] relative rounded-lg py-8",
         },
         position: {
             none: "",
@@ -32,64 +62,103 @@ const variants = cva("ring-0 outline-0 appearance-none container flex flex-col g
 
 export type DrawerProps = {
     title?: Label;
-    footer?: Label;
     open: boolean;
+    footer?: Label;
+    resizer?: boolean;
     asChild?: boolean;
-    trigger: Label | React.FC<any>;
-    onChange: (nextState: boolean) => void;
     type?: "dialog" | "drawer";
     position?: "left" | "right";
+    trigger: Label | React.FC<any>;
+    onChange: (nextState: boolean) => void;
 };
 
-export const Modal = (props: PropsWithChildren<DrawerProps>) => {
-    const type = props.type || "dialog";
-    const position = type === "dialog" ? "none" : props.position || "left";
+type DraggableProps = {
+    type: DrawerSides;
+    parent: React.RefObject<HTMLElement>;
+    value: MotionValue<number | undefined>;
+};
+
+const dragConstraints = { top: 0, left: 0, right: 0, bottom: 0 };
+
+const Draggable = (props: DraggableProps) => {
+    const handleDrag = (_: any, info: PanInfo) => {
+        if (props.parent.current) {
+            const div = props.parent.current as HTMLElement;
+            const v = props.value.get() || div.getBoundingClientRect().width;
+            const delta = props.type === "right" ? -info.delta.x : info.delta.x;
+            props.value.set(Math.abs(v + delta));
+        }
+    };
+
+    return (
+        <motion.div
+            drag="x"
+            dragElastic={0}
+            dragMomentum={false}
+            onDrag={handleDrag}
+            dragConstraints={dragConstraints}
+            whileDrag={{ cursor: "grabbing" }}
+            className={`absolute top-1/2 ${props.type === "left" ? "right-2" : "left-2"} rounded-lg cursor-grab h-10 w-2 bg-floating-border`}
+        />
+    );
+};
+
+export const Modal = ({ type = "dialog", resizer = true, ...props }: PropsWithChildren<DrawerProps>) => {
+    const isDialog = type === "dialog";
+    const position = isDialog ? "none" : props.position || "left";
+    const animation = isDialog ? animations.dialog : animations.drawer(position as DrawerSides);
     const headingId = useId();
     const descriptionId = useId();
-
-    const { refs, context } = useFloating({
-        open: props.open,
-        onOpenChange: props.onChange,
-    });
+    const { refs, context } = useFloating({ open: props.open, onOpenChange: props.onChange });
     const click = useClick(context);
     const role = useRole(context);
     const dismiss = useDismiss(context, { escapeKey: true, referencePress: true, outsidePress: false });
-
     const { getReferenceProps, getFloatingProps } = useInteractions([click, role, dismiss]);
     const Trigger = props.trigger as any;
+    const value = useMotionValue<undefined | number>(undefined);
 
     return (
         <Fragment>
             {props.asChild ? (
                 <Slot ref={refs.setReference} {...getReferenceProps()} children={Trigger} />
             ) : (
-                <button ref={refs.setReference} {...getReferenceProps()}>
+                <button ref={refs.setReference} {...getReferenceProps()} type="button">
                     {Trigger}
                 </button>
             )}
             <FloatingPortal>
-                {props.open && (
-                    <FloatingOverlay className={`relative bg-floating-overlay/70 ${type === "drawer" ? "" : "grid items-baseline p-8"}`} lockScroll>
-                        <FloatingFocusManager closeOnFocusOut context={context}>
-                            <div
-                                ref={refs.setFloating}
-                                aria-labelledby={headingId}
-                                aria-describedby={descriptionId}
-                                className={variants({ position, type })}
-                                {...getFloatingProps()}
-                            >
-                                <span className="w-5 h-2" />
-                                {props.title ? (
-                                    <header className="w-full">
-                                        <h2 className="px-8 pb-4 border-b text-3xl font-medium leading-relaxed">{props.title}</h2>
-                                    </header>
-                                ) : null}
-                                <div className="flex-1 px-8 overflow-scroll">{props.children}</div>
-                                {props.footer ? <footer className="px-8 border-t pt-4 w-full">{props.footer}</footer> : null}
-                            </div>
-                        </FloatingFocusManager>
-                    </FloatingOverlay>
-                )}
+                <AnimatePresence presenceAffectsLayout>
+                    {props.open && (
+                        <FloatingOverlay
+                            lockScroll
+                            className={`relative !overflow-clip h-[100dvh] z-floating bg-floating-overlay/70 ${type === "drawer" ? "" : "grid items-baseline p-8"}`}
+                        >
+                            <FloatingFocusManager modal closeOnFocusOut context={context}>
+                                <motion.div
+                                    animate="enter"
+                                    aria-describedby={descriptionId}
+                                    aria-labelledby={headingId}
+                                    className={variants({ position, type })}
+                                    exit="exit"
+                                    initial="initial"
+                                    ref={refs.setFloating}
+                                    variants={animation}
+                                    style={{ width: value }}
+                                    {...getFloatingProps()}
+                                >
+                                    {!isDialog && resizer ? <Draggable value={value} parent={refs.floating} type={position as DrawerSides} /> : null}
+                                    {props.title ? (
+                                        <header className="w-full">
+                                            <h2 className="px-8 pb-4 border-b text-3xl font-medium leading-relaxed">{props.title}</h2>
+                                        </header>
+                                    ) : null}
+                                    <div className="flex-1 px-8 overflow-scroll">{props.children}</div>
+                                    {props.footer ? <footer className="px-8 border-t pt-4 w-full">{props.footer}</footer> : null}
+                                </motion.div>
+                            </FloatingFocusManager>
+                        </FloatingOverlay>
+                    )}
+                </AnimatePresence>
             </FloatingPortal>
         </Fragment>
     );
