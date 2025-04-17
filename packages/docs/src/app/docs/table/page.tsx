5bf804b333f25baa853518066e679089b596e8ed
g4rcez <allan.f.garcez@gmail.com>
2025-04-17 08:53:30 -0300

    docs: table docs with invisible first line

--- packages/docs/src/app/docs/table/page.tsx
+++ packages/docs/src/app/docs/table/page.tsx
@@ -1,14 +1,16 @@
 "use client";
 import { DocsLayout } from "@/components/docs-layout";
-import { Fragment, useEffect, useState } from "react";
+import { Fragment, useCallback, useEffect, useState } from "react";
 import {
   Card,
+  Checkbox,
   ColType,
   createColumns,
   Table,
   useTablePreferences,
   uuid,
 } from "../../../../../lib/src";
+import { useStableRef } from "../../../../../lib/src/hooks/use-stable-ref";
 
 type User = { id: string; name: number; type: string; document: string };
 
@@ -23,33 +25,50 @@ const cols = createColumns<User>((col) => {
       </Fragment>
     ),
   });
-  col.add("id", "ID");
+  col.add("id", "ID", {
+    Element: (props) => (
+      <Fragment>
+        {props.value}.{props.value}.{props.value}.{props.value}
+      </Fragment>
+    ),
+  });
   col.add("type", "Type");
   col.add("document", "Document");
 });
 
-const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+const clients = Array.from({ length: 10000 }).map(
+  (_, i): User => ({
+    id: uuid(),
+    name: i,
+    type: i % 2 === 0 ? "pj" : "pf",
+    document: i % 2 === 0 ? "00000000000" : "00000000000000",
+  }),
+);
 
 export default function TablePage() {
   const preferences = useTablePreferences("@test", cols);
-  const [clients, setClients] = useState<User[]>([]);
+  const [map, setMap] = useState(new Map<string, any>());
+  const mapRef = useStableRef(map)
 
-  useEffect(() => {
-    const req = async () => {
-      await sleep(3000);
-      setClients(
-        Array.from({ length: 1000 }).map(
-          (_, i): User => ({
-            id: uuid(),
-            name: i,
-            type: i % 2 === 0 ? "pj" : "pf",
-            document: i % 2 === 0 ? "00000000000" : "00000000000000",
-          }),
-        ),
-      );
-    };
-    req();
-  }, []);
+  const Aside = useCallback(
+    (props: any) => (
+      <div className="flex h-full px-4 items-center">
+        <Checkbox
+          checked={mapRef.current.has(props.row.id)}
+          onChange={(e) => {
+            const checked = e.target.checked;
+            setMap((prev) => {
+              const clone = new Map(prev);
+              if (checked) clone.set(props.row.id, props.row);
+              else clone.delete(props.row.id);
+              return clone;
+            });
+          }}
+        />
+      </div>
+    ),
+    [map],
+  );
 
   return (
     <DocsLayout
@@ -60,11 +79,12 @@ export default function TablePage() {
       <Card container="px-0 py-0 pb-0" className="py-0 pb-0 lg:px-0 px-0">
         <Table<User>
           {...preferences}
-          loading={clients.length === 0}
-          sticky={40}
+          sticky={39}
           name="table"
+          Aside={Aside}
           rows={clients}
           operations={false}
+          loading={clients.length === 0}
         />
       </Card>
     </DocsLayout>
