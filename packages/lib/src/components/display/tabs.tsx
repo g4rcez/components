0cf13bbccb0237f9efd1e0bf2aecde3f62ccfd9c
g4rcez <allan.f.garcez@gmail.com>
2025-04-29 02:01:49 -0300

    feat: remove border-bottom animation

--- packages/lib/src/components/display/tabs.tsx
+++ packages/lib/src/components/display/tabs.tsx
@@ -1,15 +1,16 @@
 "use client";
-import { useMotionValue } from "motion/react";
+import { motion, useMotionValue } from "motion/react";
 import React, { createContext, Fragment, PropsWithChildren, useContext, useEffect, useRef } from "react";
 import { useReactive } from "../../hooks/use-reactive";
 import { useStableRef } from "../../hooks/use-stable-ref";
+import { css } from "../../lib/dom";
 import { Label } from "../../types";
 import { Polymorph } from "../core/polymorph";
 import { Card } from "./card";
 
 export type TabsProps = {
     active: string;
-    useHash?: boolean;
+    container?: string;
     className?: string;
     onChange?: (id: string) => void;
 };
@@ -18,40 +19,19 @@ const Context = createContext<string>("");
 
 export const Tabs = (props: PropsWithChildren<TabsProps>) => {
     const [active, setActive] = useReactive(props.active);
-    const left = useMotionValue(0);
-    const width = useMotionValue(0);
     const ref = useRef<HTMLDivElement | null>(null);
+    const onChangeRef = useStableRef(props.onChange);
 
     useEffect(() => {
         const header = ref.current;
         if (header === null) return;
-        const resize = (element?: HTMLElement | null) => {
-            if (!element) return;
-            const rect = element.getBoundingClientRect();
-            width.set(rect.width);
-            left.set(element.offsetLeft);
-        };
-        const listener = () => {
-            const element = header.querySelector<HTMLElement>(`li[data-active=true]`);
-            return void resize(element);
-        };
-        window.addEventListener("resize", listener);
         let first = header.querySelector<HTMLElement>(`li[data-active=true]`);
-        const hash = window.location.hash.replace(/^#/, "");
-        if (props.active === "" && hash !== "") {
-            first = header.querySelector<HTMLElement>(`li[data-id="${hash}"]`);
-            setActive(hash);
-        }
         if (first === null) {
             first = header.querySelector<HTMLElement>(`li[data-id]`)!;
             const id = first.getAttribute("data-id") || "";
             setActive(id);
         }
-        resize(first);
-        return () => window.removeEventListener("resize", listener);
-    }, []);
-
-    const onChangeRef = useStableRef(props.onChange);
+    }, [props.active, setActive]);
 
     useEffect(() => {
         if (onChangeRef.current) onChangeRef.current(active);
@@ -61,9 +41,6 @@ export const Tabs = (props: PropsWithChildren<TabsProps>) => {
 
     const onClick = (e: React.MouseEvent<HTMLAnchorElement | HTMLButtonElement>) => {
         const anchor = e.currentTarget;
-        const rect = anchor.getBoundingClientRect();
-        width.set(rect.width);
-        left.set(anchor.offsetLeft);
         setActive(anchor.dataset.id || "");
     };
 
@@ -71,26 +48,28 @@ export const Tabs = (props: PropsWithChildren<TabsProps>) => {
         <Context.Provider value={active}>
             <Card
                 className={props.className}
-                container="pt-0 max-w-full w-full min-w-0"
+                container={css("pt-0 max-w-full w-full min-w-0", props.container)}
                 header={
-                    <header ref={ref} className="relative mb-2 overflow-x-auto border-b border-card-border">
+                    <header ref={ref} className="relative mb-2 overflow-x-auto">
+                        <div className="absolute bottom-0 h-[1px] w-full bg-card-border" />
                         <nav className="min-w-0">
                             <ul className="flex w-0 min-w-full flex-1 justify-start overflow-x-auto">
                                 {items.map((x: any) => {
                                     const inner = x.props as TabProps;
+                                    const current = active === inner.id;
                                     return (
                                         <li
                                             data-id={inner.id}
+                                            data-active={current}
                                             key={`tab-header-${inner.id}`}
-                                            data-active={active === inner.id}
-                                            className="w-full border-b-2 transition-all border-card-border data-[active=true]:border-primary data-[active=true]:text-primary"
+                                            className="relative border-b border-transparent data-[active=true]:border-primary w-fit transition-all data-[active=true]:font-medium data-[active=true]:text-primary"
                                         >
                                             <Polymorph
+                                                as="button"
+                                                type="button"
                                                 onClick={onClick}
                                                 data-id={inner.id}
                                                 aria-current="page"
-                                                as={props.useHash ? "a" : "button" as any}
-                                                href={props.useHash ? `#${inner.id}` : undefined}
                                                 className="block w-full whitespace-nowrap px-10 py-4"
                                             >
                                                 {inner.title as any}
@@ -115,12 +94,11 @@ export type TabProps =
     | { id: string; title: string; label?: undefined }
     | {
           id: string;
-          title: Omit<Label, string>;
           label: string;
+          title: Omit<Label, string>;
       };
 
 export const Tab = (props: PropsWithChildren<TabProps>) => {
     const active = useTabs();
     return <Fragment>{props.id === active ? props.children : null}</Fragment>;
 };
-
